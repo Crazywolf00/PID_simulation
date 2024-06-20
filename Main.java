85a13d461b9fd263cc6372415ccb6bdb735fc184
Vaclav Vlcek <vaclav.vlcek@ext.csas.cz>
2024-06-20 16:42:53 +0200

    add args switch

--- Main.java
+++ Main.java
@@ -1,7 +1,49 @@
-public class Main {
+import java.util.List;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("PID");
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+
+        switch (args[0]) {
+            case "new":
+                System.out.println("todo new");
+                break;
+            case "safe":
+                System.out.println("todo safe");
+                break;
+            case "load":
+                System.out.println("todo load");
+                break;
+            case "stop":
+                System.out.println("todo stop");
+                break;
+            case "play":
+                System.out.println("todo play");
+                break;
+            default:
+                printUsage();
+        }
+
     }
+
+    private static void printUsage() {
+        System.out.println();
+        System.out.println("  Command Line PID_Simulation application");
+        System.out.println("==================================================");
+        System.out.println();
+        System.out.println("  Command line arguments:");
+        System.out.println("      new   Run new one");
+        System.out.println("      safe  safe currency");
+        System.out.println("      load  load last safe");
+        System.out.println("      stop  stop running simulation");
+        System.out.println("      play  run currency simulation");
+        System.out.println();
+        System.out.println();
+    }
+
 
 }
